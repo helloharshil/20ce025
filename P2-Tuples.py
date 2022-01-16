415b50ae68dba7abd9c03f154c7d12bd692d9dae
Harshil Dholakia <77793943+helloharshil@users.noreply.github.com>
2022-01-16 15:41:33 +0530

    Add files via upload

---
+++ P2-Tuples.py
@@ -0,0 +1,39 @@
+## PIP259
+## NAME - 20CE025  Harshil Dholakia     
+# Github Repository link - https://github.com/helloharshil/20ce025.git
+
+# A ) Write a Python program to create a tuple with different data types.
+
+#create tuple
+tuple = (1729,"HARSHIL","CHARUSAT",'20CE025')
+#print
+print(tuple)
+
+
+# B ) Write a Python program to create a tuple with numbers and print one item.
+
+#create tuple
+tuple1=(11,12,13,14,15)
+#print
+print(tuple1[1])
+
+
+# C ) Write a Python program to add an item in a tuple.
+
+tuple2=1,2
+tuple2=tuple2+(3,)
+print(tuple2)
+
+
+# D ) Write a Python program to convert a tuple to a string.
+
+name = ('C','H','A','R','U','S','A','T')
+str = ''.join(name)
+print(str)
+
+
+#E ) Write a Python program to find the length of a tuple.
+
+tuppp = ('C','H','A','R','U','S','A','T')
+length=len(tuppp)
+print(length)
